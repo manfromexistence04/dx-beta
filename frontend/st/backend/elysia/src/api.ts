ce37d63d5923bff1ca1e0ef4eb08530adfd4a85b
manfromexistence01 Emon <manfromexistence01@gmail.com>
2024-04-30 08:48:04 +0000

    feat: mongodb works

--- frontend/st/backend/elysia/src/api.ts
+++ frontend/st/backend/elysia/src/api.ts
@@ -1,21 +1,27 @@
 // api.ts
 import { Elysia } from 'elysia';
 import Content from '../db/index';
-import { Schema, model, connect } from 'mongoose';
+// import { Schema, model, connect } from 'mongoose';
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://sumon:sumon1234@seyaha.pzour3n.mongodb.net/ProductList?retryWrites=true&w=majority&appName=seyaha";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 const app = new Elysia();
 
 app.get('/', async (req: any, res: any) => {
 
     try {
-        await connect("mongodb+srv://sumon:sumon1234@seyaha.pzour3n.mongodb.net/ProductList?retryWrites=true&w=majority&appName=seyaha");
-        const content = await Content.find({})
-
-        if (!content) {
-          return res.status(404).json({ message: "Content not found" })
-        }
+        await mongoose.connect(uri, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     
-        res.status(200).json(content)
+        const content = await Content.find({});
+
+        // if (!content) {
+        //   return res.status(404).json({ message: "Content not found" })
+        // }
+    
+        console.log(content)
 
     } catch (err) {
         console.error(err);
