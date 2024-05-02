930eed359c5135e38d730e32ccf3394d7a7f894c
manfromexistence04 Emon <manfromexistence04@gmail.com>
2024-05-02 21:51:39 +0000

    feat: plate is working fine is admin panel

---
+++ frontend/admin-panel/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+    name: 'Admin Panle',
+    description:
+        'Rich-data editor components built with Nextjs.',
+    mainNav: [
+        {
+            title: 'Home',
+            href: '/',
+        },
+    ],
+    links: {
+        twitter: 'https://twitter.com/zbeyens',
+        github: 'https://github.com/udecode/plate',
+        docs: 'https://platejs.org',
+    },
+};
