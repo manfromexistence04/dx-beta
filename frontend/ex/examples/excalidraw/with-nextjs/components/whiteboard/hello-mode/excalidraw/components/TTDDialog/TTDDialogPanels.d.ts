b65c64677f2f3fa616c69d93993251e57fc91365
manfromexistence04 Emon <manfromexistence04@gmail.com>
2024-05-02 16:24:56 +0000

    feat: nextjs moved please work

---
+++ frontend/ex/examples/excalidraw/with-nextjs/components/whiteboard/hello-mode/excalidraw/components/TTDDialog/TTDDialogPanels.d.ts
@@ -0,0 +1,4 @@
+import { ReactNode } from "react";
+export declare const TTDDialogPanels: ({ children }: {
+    children: ReactNode;
+}) => JSX.Element;
---
+++ frontend/ex/examples/excalidraw/with-nextjs/components/whiteboard/hello-mode/excalidraw/element/transformHandles.d.ts
@@ -0,0 +1,54 @@
+import { ElementsMap, ExcalidrawElement, NonDeletedExcalidrawElement, PointerType } from "./types";
+import { Bounds } from "./bounds";
+import { Device, InteractiveCanvasAppState, Zoom } from "../types";
+export type TransformHandleDirection = "n" | "s" | "w" | "e" | "nw" | "ne" | "sw" | "se";
+export type TransformHandleType = TransformHandleDirection | "rotation";
+export type TransformHandle = Bounds;
+export type TransformHandles = Partial<{
+    [T in TransformHandleType]: TransformHandle;
+}>;
+export type MaybeTransformHandleType = TransformHandleType | false;
+export declare const DEFAULT_OMIT_SIDES: {
+    e: boolean;
+    s: boolean;
+    n: boolean;
+    w: boolean;
+};
+export declare const OMIT_SIDES_FOR_MULTIPLE_ELEMENTS: {
+    e: boolean;
+    s: boolean;
+    n: boolean;
+    w: boolean;
+};
+export declare const OMIT_SIDES_FOR_FRAME: {
+    e: boolean;
+    s: boolean;
+    n: boolean;
+    w: boolean;
+    rotation: boolean;
+};
+export declare const canResizeFromSides: (device: Device) => boolean;
+export declare const getOmitSidesForDevice: (device: Device) => {};
+export declare const getTransformHandlesFromCoords: ([x1, y1, x2, y2, cx, cy]: [number, number, number, number, number, number], angle: number, zoom: Zoom, pointerType: PointerType, omitSides?: {
+    s?: boolean | undefined;
+    e?: boolean | undefined;
+    w?: boolean | undefined;
+    n?: boolean | undefined;
+    nw?: boolean | undefined;
+    ne?: boolean | undefined;
+    sw?: boolean | undefined;
+    se?: boolean | undefined;
+    rotation?: boolean | undefined;
+}, margin?: number) => TransformHandles;
+export declare const getTransformHandles: (element: ExcalidrawElement, zoom: Zoom, elementsMap: ElementsMap, pointerType?: PointerType, omitSides?: {
+    s?: boolean | undefined;
+    e?: boolean | undefined;
+    w?: boolean | undefined;
+    n?: boolean | undefined;
+    nw?: boolean | undefined;
+    ne?: boolean | undefined;
+    sw?: boolean | undefined;
+    se?: boolean | undefined;
+    rotation?: boolean | undefined;
+}) => TransformHandles;
+export declare const shouldShowBoundingBox: (elements: readonly NonDeletedExcalidrawElement[], appState: InteractiveCanvasAppState) => boolean;
