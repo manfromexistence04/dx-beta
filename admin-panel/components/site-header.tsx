f28b74e434912fda2650c9fab2b7bc51774f4015
manfromexistence04 Emon <manfromexistence04@gmail.com>
2024-05-04 18:14:35 +0000

    feat: yeah same

--- admin-panel/components/site-header.tsx
+++ admin-panel/components/site-header.tsx
@@ -20,23 +20,6 @@ export function SiteHeader() {
             <CommandMenu />
           </div>
           <nav className="flex items-center">
-            {/* <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0"
-                )}
-              >
-                <Icons.gitHub className="h-4 w-4" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link> */}
             <Link
               href={siteConfig.links.twitter}
               target="_blank"
