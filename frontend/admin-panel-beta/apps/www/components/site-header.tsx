b22426d2b6af62eb36da94d25067db511a255396
Harry <ajju40959@gmail.com>
2024-05-15 17:41:15 +0000

    feat: just need to add theme

--- frontend/admin-panel-beta/apps/www/components/site-header.tsx
+++ frontend/admin-panel-beta/apps/www/components/site-header.tsx
@@ -11,6 +11,7 @@ import { ModeToggle } from "@/components/mode-toggle"
 import { buttonVariants } from "@/registry/new-york/ui/button"
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
@@ -37,6 +38,73 @@ import { CardsCookieSettings } from "@/registry/default/example/cards/cookie-set
 import Notifications from "./notifications"
 import { CardsReportIssue } from "@/registry/default/example/cards/report-issue"
 import { usePathname } from "next/navigation"
+import {
+  ChevronLeft,
+  ChevronRight,
+  Copy,
+  CreditCard,
+  File,
+  Home,
+  LineChart,
+  ListFilter,
+  MoreVertical,
+  Package,
+  Package2,
+  PanelLeft,
+  Search,
+  Settings,
+  ShoppingCart,
+  Truck,
+  Users2,
+} from "lucide-react"
+
+import { Badge } from "@/components/ui/badge"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { Input } from "@/components/ui/input"
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+} from "@/components/ui/pagination"
+import { Progress } from "@/components/ui/progress"
+import { Separator } from "@/components/ui/separator"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 export function SiteHeader() {
   const { sessionId } = useAuth();
@@ -98,49 +166,49 @@ export function SiteHeader() {
                       <DropdownMenuLabel>Account</DropdownMenuLabel>
                       <DropdownMenuSeparator />
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/sign-in">
+                        <Link className="w-full text-left" href="/sign-in">
                           SignIn
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/sign-up">
+                        <Link className="w-full text-left" href="/sign-up">
                           SignUp
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuSeparator />
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/sign-up">
+                        <Link className="w-full text-left" href="/sign-up">
                           Freelancer
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/sign-up">
+                        <Link className="w-full text-left" href="/sign-up">
                           Upwork
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/sign-up">
+                        <Link className="w-full text-left" href="/sign-up">
                           Fiverr
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/sign-up">
+                        <Link className="w-full text-left" href="/sign-up">
                           Youtube
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/sign-up">
+                        <Link className="w-full text-left" href="/sign-up">
                           Discord
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuSeparator />
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/whiteboard">
+                        <Link className="w-full text-left" href="/whiteboard">
                           Whiteboard
                         </Link>
                       </DropdownMenuItem>
                       <DropdownMenuItem>
-                        <Link  className="w-full text-left" href="/planner">
+                        <Link className="w-full text-left" href="/planner">
                           Planner
                         </Link>
                       </DropdownMenuItem>
@@ -190,11 +258,245 @@ export function SiteHeader() {
                       <Bell className="h-4 w-4" />
                     </div>
                   </PopoverTrigger>
-                  <PopoverContent className="w-[425px] !pt-0 pb-3 px-3">
+                  <PopoverContent className="w-[425px] p-3">
                     {/* <CardsReportIssue />
                     <CardsCookieSettings /> */}
-                    <Notifications />
-
+                    {/* <Notifications /> */}
+                    <Tabs defaultValue="week">
+                      <div className="flex items-center">
+                        <TabsList>
+                          <TabsTrigger value="week">All</TabsTrigger>
+                          <TabsTrigger value="month">Archive</TabsTrigger>
+                          <TabsTrigger value="year">Comments</TabsTrigger>
+                        </TabsList>
+                        <div className="ml-auto flex items-center gap-2">
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button
+                                variant="outline"
+                                size="sm"
+                                className="h-7 gap-1 text-sm"
+                              >
+                                <Settings className="h-3.5 w-3.5" />
+                                <span className="sr-only sm:not-sr-only">Settings</span>
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                              <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                              <DropdownMenuSeparator />
+                              <DropdownMenuCheckboxItem checked>
+                                Fulfilled
+                              </DropdownMenuCheckboxItem>
+                              <DropdownMenuCheckboxItem>
+                                Declined
+                              </DropdownMenuCheckboxItem>
+                              <DropdownMenuCheckboxItem>
+                                Refunded
+                              </DropdownMenuCheckboxItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                          {/* <Button
+                            size="sm"
+                            variant="outline"
+                            className="h-7 gap-1 text-sm"
+                          >
+                            <File className="h-3.5 w-3.5" />
+                            <span className="sr-only sm:not-sr-only">Export</span>
+                          </Button> */}
+                        </div>
+                      </div>
+                      <TabsContent value="week">
+                        <Card x-chunk="dashboard-05-chunk-3">
+                          <CardHeader className="px-7">
+                            <CardTitle>Orders</CardTitle>
+                            <CardDescription>
+                              Recent orders from your store.
+                            </CardDescription>
+                          </CardHeader>
+                          <CardContent>
+                            <Table>
+                              <TableHeader>
+                                <TableRow>
+                                  <TableHead>Customer</TableHead>
+                                  <TableHead className="hidden sm:table-cell">
+                                    Type
+                                  </TableHead>
+                                  <TableHead className="hidden sm:table-cell">
+                                    Status
+                                  </TableHead>
+                                  <TableHead className="hidden md:table-cell">
+                                    Date
+                                  </TableHead>
+                                  <TableHead className="text-right">Amount</TableHead>
+                                </TableRow>
+                              </TableHeader>
+                              <TableBody>
+                                <TableRow className="bg-accent">
+                                  <TableCell>
+                                    <div className="font-medium">Liam Johnson</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      liam@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Sale
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="secondary">
+                                      Fulfilled
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-23
+                                  </TableCell>
+                                  <TableCell className="text-right">$250.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                  <TableCell>
+                                    <div className="font-medium">Olivia Smith</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      olivia@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Refund
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="outline">
+                                      Declined
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-24
+                                  </TableCell>
+                                  <TableCell className="text-right">$150.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                  <TableCell>
+                                    <div className="font-medium">Noah Williams</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      noah@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Subscription
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="secondary">
+                                      Fulfilled
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-25
+                                  </TableCell>
+                                  <TableCell className="text-right">$350.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                  <TableCell>
+                                    <div className="font-medium">Emma Brown</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      emma@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Sale
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="secondary">
+                                      Fulfilled
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-26
+                                  </TableCell>
+                                  <TableCell className="text-right">$450.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                  <TableCell>
+                                    <div className="font-medium">Liam Johnson</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      liam@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Sale
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="secondary">
+                                      Fulfilled
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-23
+                                  </TableCell>
+                                  <TableCell className="text-right">$250.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                  <TableCell>
+                                    <div className="font-medium">Liam Johnson</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      liam@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Sale
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="secondary">
+                                      Fulfilled
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-23
+                                  </TableCell>
+                                  <TableCell className="text-right">$250.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                  <TableCell>
+                                    <div className="font-medium">Olivia Smith</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      olivia@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Refund
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="outline">
+                                      Declined
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-24
+                                  </TableCell>
+                                  <TableCell className="text-right">$150.00</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                  <TableCell>
+                                    <div className="font-medium">Emma Brown</div>
+                                    <div className="hidden text-sm text-muted-foreground md:inline">
+                                      emma@example.com
+                                    </div>
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    Sale
+                                  </TableCell>
+                                  <TableCell className="hidden sm:table-cell">
+                                    <Badge className="text-xs" variant="secondary">
+                                      Fulfilled
+                                    </Badge>
+                                  </TableCell>
+                                  <TableCell className="hidden md:table-cell">
+                                    2023-06-26
+                                  </TableCell>
+                                  <TableCell className="text-right">$450.00</TableCell>
+                                </TableRow>
+                              </TableBody>
+                            </Table>
+                          </CardContent>
+                        </Card>
+                      </TabsContent>
+                    </Tabs>
                   </PopoverContent>
                 </Popover>
 
