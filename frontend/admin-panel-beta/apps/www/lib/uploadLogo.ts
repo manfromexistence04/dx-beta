90e04b8ab9209b2d7f18a1cd65ae88eea230caad
Harry <ajju40959@gmail.com>
2024-05-15 22:15:41 +0000

    feat: vercel deployment of new admin panel

--- frontend/admin-panel-beta/apps/www/lib/uploadLogo.ts
+++ frontend/admin-panel-beta/apps/www/lib/uploadLogo.ts
@@ -1,6 +1,6 @@
 import { generateReactHelpers } from "@uploadthing/react"
 
-import type { OurFileRouter } from "@/app/api/uploadthing/logo"
+import type { OurFileRouter } from "@/app/(app)/api/uploadthing/logo"
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>()
