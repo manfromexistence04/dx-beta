eb331d44d072f578996188a981680dcf633289a0
manfromexistence02 Emon <manfromexistence02@gmail.com>
2024-05-17 14:41:42 +0000

    feat: admin-panel deploy problem

--- frontend/www/admin-panel/next.config.js
+++ frontend/www/admin-panel/next.config.js
@@ -100,41 +100,9 @@
  */
 
 /** @type {import("next").NextConfig} */
-// const config = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "utfs.io",
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'img.clerk.com',
-//         port: '',
-//       },
-//     ],
-//     unoptimized: true,
-//   },
-// }
-
-// module.exports = config
-
-import { createContentlayerPlugin } from "next-contentlayer"
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const config = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
       {
         protocol: "https",
         hostname: "utfs.io",
@@ -145,50 +113,82 @@ const nextConfig = {
         port: '',
       },
     ],
+    unoptimized: true,
   },
-  // redirects() {
-  //   return [
-  //     {
-  //       source: "/components",
-  //       destination: "/docs/components/accordion",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/docs/components",
-  //       destination: "/docs/components/accordion",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/examples",
-  //       destination: "/examples/mail",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/docs/primitives/:path*",
-  //       destination: "/docs/components/:path*",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/figma",
-  //       destination: "/docs/figma",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/docs/forms",
-  //       destination: "/docs/components/form",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/docs/forms/react-hook-form",
-  //       destination: "/docs/components/form",
-  //       permanent: false,
-  //     },
-  //   ]
-  // },
 }
 
-const withContentlayer = createContentlayerPlugin({
-  // Additional Contentlayer config options
-})
+module.exports = config
 
-export default withContentlayer(nextConfig)
+// import { createContentlayerPlugin } from "next-contentlayer"
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "avatars.githubusercontent.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "utfs.io",
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'img.clerk.com',
+//         port: '',
+//       },
+//     ],
+//   },
+//   // redirects() {
+//   //   return [
+//   //     {
+//   //       source: "/components",
+//   //       destination: "/docs/components/accordion",
+//   //       permanent: true,
+//   //     },
+//   //     {
+//   //       source: "/docs/components",
+//   //       destination: "/docs/components/accordion",
+//   //       permanent: true,
+//   //     },
+//   //     {
+//   //       source: "/examples",
+//   //       destination: "/examples/mail",
+//   //       permanent: false,
+//   //     },
+//   //     {
+//   //       source: "/docs/primitives/:path*",
+//   //       destination: "/docs/components/:path*",
+//   //       permanent: true,
+//   //     },
+//   //     {
+//   //       source: "/figma",
+//   //       destination: "/docs/figma",
+//   //       permanent: true,
+//   //     },
+//   //     {
+//   //       source: "/docs/forms",
+//   //       destination: "/docs/components/form",
+//   //       permanent: false,
+//   //     },
+//   //     {
+//   //       source: "/docs/forms/react-hook-form",
+//   //       destination: "/docs/components/form",
+//   //       permanent: false,
+//   //     },
+//   //   ]
+//   // },
+// }
+
+// const withContentlayer = createContentlayerPlugin({
+//   // Additional Contentlayer config options
+// })
+
+// export default withContentlayer(nextConfig)
